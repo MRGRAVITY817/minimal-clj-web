c9c18cd4a7035c11e3601ec71a2e57d389b1f86b
Hoon Wee <mrgravity817@gmail.com>
2024-01-13 00:40:28 +0900

    feat: routing with req

--- src/minimal/core.clj
+++ src/minimal/core.clj
@@ -6,9 +6,20 @@
 (defonce server (atom nil))
 
 (defn app [req]
-  {:status 200
-   :body (with-out-str (pprint/pprint req))
-   :headers {"Content-Type" "text/plain"}})
+  (case (:uri req)
+    "/" {:status 200
+         :body "<h1>Homepage</h1>
+                <ul>
+                    <li><a href=\"/echo\">Echo request</a></li>
+                    <li><a href=\"/greeting\">Greeting</a></li>
+                </ul>"
+         :headers {"Content-Type" "text/html; charset=UTF-8"}}
+    "/echo" {:status 200
+             :body (with-out-str (pprint/pprint req))
+             :headers {"Content-Type" "text/plain"}}
+    {:status 404
+     :body "Not found."
+     :headers {"Content-Type" "text/plain"}}))
 
 (defn start-server []
   (reset! server
